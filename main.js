3834ad7ef7ba5191a43e1d07c113114acbd3148e
henriqueganz <henrique.ganz@gmail.com>
2023-02-02 18:08:41 -0300

    Motel vison Final

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
-            entry.target.classList.add('show')
+            entry.target.classList.add('show-animate')
         }else {
             entry.target.classList.remove('show');
         }
